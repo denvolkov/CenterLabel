00292fe83174bd99307a29b08c92befeca278b4f
denvolkov <denvolkov@inbox.ru>
2020-12-11 14:37:27 +0300

    Added element

---
+++ CenterLabel/SimpleSound.swift
@@ -0,0 +1,51 @@
+//
+//  SimpleSound.swift
+//  CenterLabel
+//
+//  Created by test on 11.12.2020.
+//
+
+import Foundation
+import AudioToolbox
+
+class SimpleSound {
+    private var soundID: SystemSoundID = 0
+    
+    public init (name: String) {
+        if let soundURL = soundURL (forName: name) {
+            let status =
+            AudioServicesCreateSystemSoundID(soundURL as CFURL, &soundID)
+            if status != noErr {
+                print("Unable to create sound at URL: '\(name)'")
+                soundID = 0
+            }
+        }
+    }
+    
+    public func play() {
+        if soundID != 0 {
+            AudioServicesPlaySystemSound(soundID)
+        }
+    }
+    
+    private func soundURL(forName name: String) -> URL?
+    {
+        let fileExtensions = ["m4a", "wav" , "mp3"]
+     
+        for fileExtention in fileExtensions {
+            if let soundURL =
+                Bundle.main.url(forResource: name, withExtension: fileExtention) {
+                return soundURL
+            }
+        }
+        
+        print("Unable to find sound file with name '\(name)'")
+        return nil
+    }
+    
+    deinit {
+        if soundID != 0 {
+            AudioServicesDisposeSystemSoundID(soundID)
+        }
+    }
+}
--- CenterLabel/ViewController.swift
+++ CenterLabel/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let elementList = ["Honey", "Bee"]
+    var currentElementIndex = 0
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    let bankaSound = SimpleSound(name: "banka")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateElement()
     }
-
-
+    
+    func updateElement() {
+        
+        answerLabel.text = "?"
+        let elementName = elementList[currentElementIndex]
+        let image = UIImage(named:elementName)
+        imageView.image = image
+        
+    }
+    
+    
+    @IBAction func showAnswerButtinDidPressed(_ sender: UIButton) {
+        answerLabel.text = elementList[currentElementIndex]
+    }
+    
+    @IBAction func nextElementButtonDidPressed(_ sender: UIButton) {
+        currentElementIndex += 1
+        if currentElementIndex >= elementList.count {
+            currentElementIndex = 0
+        }
+        
+        updateElement()
+    }
+    
+    @IBAction func soundButtonDidPressed(_ sender: UIButton) {
+        bankaSound.play()
+    }
 }
 
